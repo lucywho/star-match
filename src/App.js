a86bdb7ee2fe685566c19cd1b452af85e2621cba
Lucy Toman <lucy@toman.me.uk>
2022-11-10 12:16:41 +0100

    adds stars and buttons, renders random no. stars

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { utils, colors } from "./utils"
+
 function App() {
+    const stars = utils.random(1, 9)
     return (
-        <div className="App">
+        <>
             <header>Star Match Game</header>
-        </div>
+
+            <div className="game">
+                <div className="help">
+                    Pick 1 or numbers that sum to the number of stars
+                </div>
+                <div className="body">
+                    <div className="left">
+                        {utils.range(1, stars).map((starId) => (
+                            <div
+                                key={starId}
+                                className="star"
+                                style={{ color: colors.used }}
+                            ></div>
+                        ))}
+                    </div>
+                    <div className="right">
+                        {utils.range(1, 9).map((number, numId) => (
+                            <button key={numId} className="number">
+                                {number}
+                            </button>
+                        ))}
+                    </div>
+                </div>
+                <div className="timer">Time Remaining: 10</div>
+            </div>
+        </>
     )
 }
 
